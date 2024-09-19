2efcbba4e22be70c1ea143adabbb23415f5fa903
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-09-19 10:24:50 +0300

    renamed hypnode to example

--- src/main/java/org/compiler/cli/Main.java
+++ src/main/java/org/compiler/cli/Main.java
@@ -4,8 +4,8 @@ import picocli.CommandLine;
 
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "hn2c", mixinStandardHelpOptions = true,
-        description = "Converts Hypnode programs to C code", version = "0.1")
+@CommandLine.Command(name = "example", mixinStandardHelpOptions = true,
+        description = "Converts example programs to C code", version = "0.1")
 public class Main implements Callable<Integer> {
 
     @Override
